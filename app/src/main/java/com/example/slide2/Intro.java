125c5392649f0874b866b77c141d781c4f5639ee
mlathif99 <mohlathif99@gmail.com>
2022-09-14 11:04:01 +0700

    first commit

---
+++ app/src/main/java/com/example/slide2/Intro.java
@@ -0,0 +1,160 @@
+package com.example.slide2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class Intro extends AppCompatActivity {
+
+    private static final int MAX_STEP = 6;
+
+    private Button btn_got_it;
+    private String[] title_array = {
+            "Mode Developer", "Fake GPS",
+            "Face Detection", "Technology",
+            "History","Utility"
+    };
+    private String[] description_array = {
+            "Pengembangan Aplikasi GoodDay", "GoodDay Tidak dapat menggunakan Fitur Fake GPS",
+            "Wajah tidak sesuai data,GoodDay tidak dapat menerima", "Penggunaan teknologi untuk menunjang GoodDay",
+            "Seluruh riwayat pengguna tersimpan pada sistem GoodDay","GoodDay akan terus dikembangkan dengan tingkat kompleksitas yang tinggi "
+    };
+    private int[] about_images_array = {
+            R.drawable.developer, R.drawable.fakegps,
+            R.drawable.facedetection1, R.drawable.tecno1,
+            R.drawable.history, R.drawable.utility
+    };
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        initComponent();
+    }
+
+    private void initComponent() {
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        btn_got_it = findViewById(R.id.btn_got_it);
+
+        bottomProgressDots(0);
+
+        MyViewPagerAdapter myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        btn_got_it.setVisibility(View.GONE);
+        btn_got_it.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intro.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_skip).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intro.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void bottomProgressDots(int index) {
+        LinearLayout dotsLayout = findViewById(R.id.layoutDots);
+        ImageView[] dots = new ImageView[MAX_STEP];
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new ImageView(this);
+            int width_height = 15;
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(width_height, width_height));
+            params.setMargins(10, 10, 10, 10);
+            dots[i].setLayoutParams(params);
+            dots[i].setImageResource(R.drawable.dot);
+            dots[i].setColorFilter(getResources().getColor(R.color.colorWhite), PorterDuff.Mode.SRC_IN);
+            dotsLayout.addView(dots[i]);
+        }
+
+        dots[index].setImageResource(R.drawable.dot);
+        dots[index].setColorFilter(getResources().getColor(R.color.colorGold), PorterDuff.Mode.SRC_IN);
+    }
+
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(final int position) {
+            bottomProgressDots(position);
+            if (position == title_array.length - 1) {
+                btn_got_it.setVisibility(View.VISIBLE);
+            } else {
+                btn_got_it.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+
+    public class MyViewPagerAdapter extends PagerAdapter {
+
+        MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(R.layout.item_intro, container, false);
+            ((TextView) view.findViewById(R.id.title)).setText(title_array[position]);
+            ((TextView) view.findViewById(R.id.description)).setText(description_array[position]);
+            ((ImageView) view.findViewById(R.id.image)).setImageResource(about_images_array[position]);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return title_array.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+}
